628a6859ad4317ee483d9aca615f093ec97f6ece
krishvachhani <155625378+krishvachhani@users.noreply.github.com>
2024-02-14 21:56:56 +0530

    GraphQL Completed

--- graphQL/index.js
+++ graphQL/index.js
@@ -27,7 +27,37 @@ async function getJobPosting() {
 }
 
 async function getSubOpening() {
-    const query = 'SELECT * FROM SubOpening';
+    const query = 'SELECT * FROM subopening';
+    return await queryAsync(query);
+}
+
+async function getExpertise() {
+    const query = 'SELECT * FROM expertise';
+    return await queryAsync(query);
+}
+
+async function getFamiliarity() {
+    const query = 'SELECT * FROM familiarity';
+    return await queryAsync(query);
+}
+
+async function getPersonalInformation() {
+    const query = 'SELECT * FROM personalInformation';
+    return await queryAsync(query);
+}
+
+async function getApplication() {
+    const query = 'SELECT * FROM Application';
+    return await queryAsync(query);
+}
+
+async function getInformation() {
+    const query = 'SELECT * FROM Information';
+    return await queryAsync(query);
+}
+
+async function getPreference() {
+    const query = 'SELECT * FROM Preferences';
     return await queryAsync(query);
 }
 
@@ -40,18 +70,99 @@ const resolvers = {
             const users = await getUsers();
             return users.find((user) => String(user.userId) === args.id);
         },
-        async jobpostings() {
+        async jobPostings() {
             return await getJobPosting();
         },
-        async jobposting(_, args) {
+        async jobPosting(_, args) {
             const JobPostings = await getJobPosting();
             return JobPostings.find((JobPosting) => String(JobPosting.jobId) === args.id);
+        },
+        async expertises() {
+            return await getExpertise();
+        },
+        async expertise(_, args) {
+            const expertise = await getExpertise();
+            return expertise.find((user) => String(user.userId) === args.id);
+        },
+        async familiarities() {
+            return await getFamiliarity();
+        },
+        async familiarity(_, args) {
+            const familiarities = await getFamiliarity();
+            return familiarities.find((familiarity) => String(familiarity.userId) === args.id);
+        },
+        async allInformation() {
+            return await getInformation();
+        },
+        async information(_, args) {
+            const allInformation = await getInformation();
+            return allInformation.find((information) => String(information.userId) === args.id);
+        },
+        async preferences() {
+            return await getPreference();
+        },
+        async preference(_, args) {
+            const preferences = await getPreference();
+            return preferences.find((preference) => String(preference.jobId) === args.id);
+        },
+        async allPersonalInformation() {
+            return await getPersonalInformation();
+        },
+        async personalInformation(_, args) {
+            const allPersonalInformation = await getPersonalInformation();
+            return allPersonalInformation.find((personalInformation) => String(personalInformation.userId) === args.id);
+        },
+        async subOpenings() {
+            return await getSubOpening();
+        },
+        async subOpening(_, args) {
+            const subOpenings = await getSubOpening();
+            return subOpenings.find((subOpening) => String(subOpening.openingId) === args.id);
+        },
+        async applications() {
+            return await getApplication();
+        },
+        async application(_, args) {
+            const applications = await getApplication();
+            return applications.find((application) => String(application.applicationId) === args.id);
+        }
+    },
+    User: {
+        async expertise(parent) {
+            const expertises = await getExpertise();
+            return expertises.find((expertise) => String(expertise.userId) === String(parent.userId));
+        },
+        async familiarity(parent) {
+            const familiarities = await getFamiliarity();
+            return familiarities.find((familiarity) => String(familiarity.userId) === String(parent.userId));
+        },
+        async personalInformation(parent) {
+            const allPersonalInformation = await getPersonalInformation();
+            return allPersonalInformation.find((personalInformation) => String(personalInformation.userId) === String(parent.userId));
+        },
+        async application(parent) {
+            const allApplication = await getApplication();
+            return allApplication.filter((application) => String(application.userId) === String(parent.userId));
+        },
+        async information(parent) {
+            const allInformation = await getInformation();
+            return allInformation.find((information) => String(information.userId) === String(parent.userId));
         }
     },
     JobPosting: {
         async subOpening(parent) {
             const SubOpenings = await getSubOpening();
-            return SubOpenings.filter((SubOpening) => (SubOpening.jobId) === (parent.jobId));
+            return SubOpenings.filter((SubOpening) => String(SubOpening.jobId) === String(parent.jobId));
+        },
+        async preferences(parent) {
+            const preferences = await getPreference();
+            return preferences.find((preference) => String(preference.jobId) === String(parent.jobId));
+        }
+    },
+    SubOpening: {
+        async application(parent) {
+            const applications = await getApplication();
+            return applications.filter((application) => String(application.openingId) === String(parent.openingId));
         }
     }
 };
--- graphQL/schema.js
+++ graphQL/schema.js
@@ -1,64 +1,108 @@
 export const typeDefs = `#graphql
-    type User{
-        userId: ID!
-        email: String
-        fullname: String
-        hashedPassword: String
-        
-    }
-    type Information{
-        percentage: Int
-        yearOfPassing: Int
-        collegeName: String
-        qualification: String
-        stream: String
-        location: String
-        yearOfExperience: Int
-        currentCTC: Int
-        expectedCTC: Int
-        phoneNumber: String
-        applicantType: String
-        noticePeriod: Int
-        noticePeriodDuration: Int
-        noticePeriodEnd: String
-        previouslyApplied: Int
-        previouslyAppliedRole: String
-        referrer: String
-    }
-    type Preferences{
-        jobId: JobPosting
-        instructionalDesigner:Int
-        softwareEngineer:Int
-        qualityEngineer:Int
-    }
-    type SubOpening{
-        openingId: ID!
-        jobId:ID!
-        jobRole: String
-        compensation: Int
-        description: String
-        requirements: String
-    }
-    type JobPosting{
-        jobId: ID!
-        startDate: String
-        expirationDate: String
-        location: String  
-        subOpening: [SubOpening]
-    }
-    type Application{
-        applicationId: String
-        opening: SubOpening
-        user: User
-        timeSlot: String
-        resume: String
-    }
-    type Query{
-        users: [User]
-        user(id:ID!): User
-        jobpostings: [JobPosting]
-        jobposting(id:ID!): JobPosting
-    }
-    
+type User{
+    userId: ID!
+    email: String
+    hashedPassword: String
+    fullname: String
+    expertise:Expertise!
+    familiarity:Familiarity!
+    personalInformation:PersonalInformation!
+    application:[Application!]
+    information:Information!
+}
+
+type Expertise{
+    userId:ID!
+    Javascript: Int
+    NodeJs:Int
+    AngularJs: Int
+    ReactJs: Int
+}
+
+type Familiarity{
+    userId:ID!
+    Javascript: Int
+    NodeJs:Int
+    AngularJs: Int
+    ReactJs: Int
+}
+type Information{
+    userId:ID!
+    applicantType: String
+    yearsOfExperience: Int
+    currentCTC: Int
+    expectedCTC: Int
+    noticePeriod: Int
+    noticePeriodDuration: Int
+    noticePeriodEnd: String
+    previouslyApplied: Int
+    previouslyAppliedRole: String
+    referrer: String
+    percentage: Int
+    yearOfPassing: Int
+    collegeName: String
+    qualification: String
+    stream: String
+    city:String
+}
+type Preferences{
+    jobId: ID!
+    InstructionalDesigner:Int
+    SoftwareEngineer:Int
+    QualityEngineer:Int
+}
+type PersonalInformation{
+    userId:ID!
+    phoneNumber:String
+    portfolioLink:String
+    resumeLink:String
+}
+type SubOpening{
+    openingId: ID!
+    jobId:ID!
+    jobRole: String
+    compensation: Int
+    description: String
+    requirements: String
+    application: [Application!]
+}
+
+type JobPosting{
+    jobId: ID!
+    startDate: String
+    expirationDate: String
+    location: String
+    subOpening: [SubOpening!]
+    preferences: Preferences!
+}
+
+type Application{
+    applicationId: ID!
+    openingId:ID!
+    userId: ID!
+    timeSlot: String
+    resume: String
+}
+
+type Query {
+    users: [User]
+    user(id: ID!): User
+    expertises: [Expertise]
+    expertise(id: ID!): Expertise
+    familiarities: [Familiarity]
+    familiarity(id: ID!): Familiarity
+    allInformation: [Information]
+    information(id: ID!): Information
+    preferences: [Preferences]
+    preference(id: ID!): Preferences
+    allPersonalInformation: [PersonalInformation]
+    personalInformation(id: ID!): PersonalInformation
+    subOpenings: [SubOpening]
+    subOpening(id: ID!): SubOpening
+    jobPostings: [JobPosting]
+    jobPosting(id: ID!): JobPosting
+    applications: [Application]
+    application(id: ID!): Application
+}
 `;
 
